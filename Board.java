0f3458ca7e58f47f41cf76c7c9102c3550adfd38
tapaswi08 <87767842+tapaswi08@users.noreply.github.com>
2021-12-12 23:53:14 +0530

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,23 @@
+public class Board {
+    private int size;
+    private int start;
+    private int end;
+
+    public Board(int size) {
+        this.start = 1;
+        this.end = start + size - 1;
+        this.size = size;
+    }
+    
+    public int getStart(){
+        return start;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
