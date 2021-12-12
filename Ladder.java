f21a546430801a484734c4ae3f5aaa9e9f895c53
tapaswi08 <87767842+tapaswi08@users.noreply.github.com>
2021-12-12 23:49:02 +0530

    Create Ladder.java

---
+++ Ladder.java
@@ -0,0 +1,18 @@
+public class Ladder {
+
+    private int start;
+    private int end;
+
+    public Ladder(int ladderStart, int ladderEnd) {
+        ladderStart = start;
+        ladderEnd = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
